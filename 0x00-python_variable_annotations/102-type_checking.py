725797903d1105d2522d36a624a0a28c08708871
Ailwei <Ailweimaemu@gmail.com>
2024-05-02 15:48:39 +0000

    102

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
-"""
 
 """
+Use mypy to validate the following piece of code
+and apply any necessary changes.
+"""
+
+from typing import Tuple, List, Any
 
 
-from typing import Tuple, Any
-
-
-def zoom_array(lst: Tuple[Any, ...], factor: int = 2) -> Tuple[Any, ...]:
-    """
-    tupple
-    """
-    zoomed_in: Tuple[Any, ...] = tuple(
+def zoom_array(lst: Tuple[Any, ...], factor: int = 2) -> List[Any]:
+    """Corrected annotations"""
+    zoomed_in: List[Any] = [
         item for item in lst
         for i in range(factor)
-    )
+    ]
     return zoomed_in
 
 
