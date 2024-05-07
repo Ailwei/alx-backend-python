5e5ba109b20be1f56ce1cf31e3846f0e5a8192ac
Ailwei <Ailweimaemu@gmail.com>
2024-05-07 15:22:22 +0000

    2. Run time for four parallel comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
-"""
-Import async_generator from previous task and define async_comprehension
-
-Collects 10 random numbers using async comprehension over async_generator.
-
-"""
+'''
+Imports async_generator from '0-async_generator'
+and defines async_comprehension coroutine.
+'''
 
 from typing import List
-async_generator = __import__('0-async_generator').async_generator
+from importlib import import_module as using
+
+async_generator = using('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """Return 10 random numbers collected asynchronously."""
-    results = [i async for i in async_generator()]
-    return results
+    '''
+    Creates a list of 10 random numbers
+    using async comprehension over async_generator.
+
+    Returns:
+        List[float]: A list containing 10 random numbers.
+    '''
+    return [num async for num in async_generator()]
